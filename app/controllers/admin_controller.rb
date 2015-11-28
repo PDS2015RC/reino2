928d415e3ba35a2fa0ea5f3dd7fc868a6850cd8b
Julian Sansat <kerarchie@yahoo.com.ar>
2015-11-28 15:51:28 -0300

    fixed permission restrictions

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,6 +1,20 @@
 class AdminController < ApplicationController
-  def index
-  	@users = User.all
-  	@chars = Character.all
-  end
+	before_action :admin_check
+  # GET /lessons
+  # GET /lessons.json
+    def admin_check
+
+    	if current_user.admin == false
+
+      		redirect_to main_index_path
+
+    	end
+	end
+	
+	def index
+		@users = User.all
+  		@chars = Character.all
+	end
+
 end
+
