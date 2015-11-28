85bec39d300f99270627a87a9244f58059d596fd
Julian Sansat <kerarchie@yahoo.com.ar>
2015-11-28 15:36:51 -0300

    added the class line_badge

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151128174852) do
+ActiveRecord::Schema.define(version: 20151128181229) do
 
   create_table "badges", force: :cascade do |t|
     t.string   "issuer_url",          limit: 255
@@ -82,6 +82,16 @@ ActiveRecord::Schema.define(version: 20151128174852) do
     t.datetime "updated_at",             null: false
   end
 
+  create_table "line_badges", force: :cascade do |t|
+    t.integer  "badge_id",   limit: 4
+    t.integer  "user_id",    limit: 4
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
+  end
+
+  add_index "line_badges", ["badge_id"], name: "index_line_badges_on_badge_id", using: :btree
+  add_index "line_badges", ["user_id"], name: "index_line_badges_on_user_id", using: :btree
+
   create_table "line_items", force: :cascade do |t|
     t.integer  "Item_id",      limit: 4
     t.integer  "Inventory_id", limit: 4
@@ -127,4 +137,6 @@ ActiveRecord::Schema.define(version: 20151128174852) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "line_badges", "badges"
+  add_foreign_key "line_badges", "users"
 end
